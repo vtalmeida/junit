7c76fe2b2ea4ca2eb76c9ce07d6795f90578a6e2
vtalmeida <cruiser.vitor@gmail.com>
2024-12-17 20:21:19 -0300

    Classe SimuladorEsperaTest - Ignorando execução dos testes condicionalmente com Assumptions e Annotations

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -2,13 +2,17 @@ package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
 class SimuladorEsperaTest {
 
     @Test
+//  @Disabled("Não é mais aplicável")
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     void deveEsperarENaoDarTimeout() {
+//      Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")), () -> "Abortando teste: não deve ser utilizado em PROD");
         Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
     }
 }
