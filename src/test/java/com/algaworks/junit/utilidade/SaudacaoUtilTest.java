baf44b49b0e13442687ea315b208a22d2ff941e3
vtalmeida <cruiser.vitor@gmail.com>
2024-12-16 21:32:26 -0300

    Commit inicial com a classe SaudacaoUtilTest

---
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -0,0 +1,20 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaudacaoUtilTest {
+
+    @Test
+    public void saudar() {
+        String saudacao = SaudacaoUtil.saudar(9);
+        assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+    }
+
+    @Test
+    public void deveLancarException() {
+        assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
+    }
+
+}
