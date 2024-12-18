831eb503edb3b4b94750d9735f907ad89a920cb1
vtalmeida <cruiser.vitor@gmail.com>
2024-12-17 21:18:20 -0300

    Mostrando o funcionamento das annotations @BeforeAll, @AfterAll, @BeforeEach e @AfterEach na classe CalculadoraGanhosTest.

---
+++ src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
@@ -0,0 +1,62 @@
+package com.algaworks.junit.blog.negocio;
+
+import com.algaworks.junit.blog.modelo.Editor;
+import com.algaworks.junit.blog.modelo.Ganhos;
+import com.algaworks.junit.blog.modelo.Post;
+import com.algaworks.junit.blog.utilidade.ProcessadorTextoSimples;
+import org.junit.jupiter.api.*;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculadoraGanhosTest {
+
+//    @AfterAll
+//    static void afterAll() {
+//        System.out.println("Depois de todos os testes");
+//    }
+//
+//    @AfterEach
+//    void afterEach() {
+//        System.out.println("Depois de cada método");
+//    }
+
+    static CalculadoraGanhos calculadora;
+    Editor autor;
+    Post post;
+
+    @BeforeAll
+    static void beforeAll() {
+        calculadora = new CalculadoraGanhos(new ProcessadorTextoSimples(), BigDecimal.TEN);
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        autor = new Editor(1L, "Alex", "alex@gmail.com", new BigDecimal(5), true);
+
+        post = new Post(1L, "Ecossistema Java", "O ecossistema do Java é muito maduro", autor,
+                "ecossistema-java-abc123", null, false, false);
+    }
+
+    @Test
+    public void deveCalcularGanhos() {
+        Ganhos ganhos = calculadora.calcular(post);
+
+        assertEquals(new BigDecimal("45"), ganhos.getTotalGanho());
+        assertEquals(7, ganhos.getQuantidadePalavras());
+        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+    }
+
+    @Test
+    public void deveCalcularGanhosSemPremium() {
+        autor.setPremium(false);
+
+        Ganhos ganhos = calculadora.calcular(post);
+
+        assertEquals(new BigDecimal("35"), ganhos.getTotalGanho());
+        assertEquals(7, ganhos.getQuantidadePalavras());
+        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+    }
+
+}
